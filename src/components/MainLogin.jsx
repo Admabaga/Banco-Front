3dfe69076479b0ebaa250098872eb0540efe6f8f
Admabaga <admabaga@outlook.com>
2024-10-22 12:21:06 -0500

    Test probando cors

--- src/components/MainLogin.jsx
+++ src/components/MainLogin.jsx
@@ -18,7 +18,7 @@ function CartaMainLogin() {
     evento.preventDefault();
     setCargando(true)
     try {
-      const response = await fetch('http://localhost:8080/usuarios/log', {
+      const response = await fetch('https://banco-backend-znok.onrender.com/usuarios/log', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
